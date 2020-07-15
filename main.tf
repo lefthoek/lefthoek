67f22cc82c6c78c53e96d17f261244ea7603044d
Jan Hein Hoogstad <yeehaa@offcourse.io>
2020-07-16 00:43:32 +0200

    change

--- main.tf
+++ main.tf
@@ -5,19 +5,8 @@ terraform {
 
     # The name of the Terraform Cloud workspace to store Terraform state files in.
     workspaces {
-      name = "general"
+      name = "infra"
     }
   }
 }
 
-provider "aws" {
-  profile = "default"
-  region  = "us-east-1"
-}
-
-# An example resource that does nothing.
-resource "null_resource" "example" {
-  triggers = {
-    value = "A example resource that does nothing!"
-  }
-}
