d8362bf058018b78ed3bde2e40d9e95b2258eb35
Jan Hein Hoogstad <yeehaa@offcourse.io>
2020-07-18 22:34:28 +0200

    add cloudfront

--- main.tf
+++ main.tf
@@ -20,7 +20,6 @@ variable "www_domain_name" {
   default = "brandbook.lefthoek.com"
 }
 
-// We'll also need the root domain (also known as zone apex or naked domain).
 variable "root_domain_name" {
   default = "lefthoek.com"
 }
@@ -53,11 +52,56 @@ POLICY
 }
 
 resource "aws_acm_certificate" "certificate" {
-  // We want a wildcard cert so we can host subdomains later.
   domain_name       = "*.${var.root_domain_name}"
   validation_method = "DNS"
-
-  // We also want the cert to be valid for the root domain even though we'll be
-  // redirecting to the www. domain immediately.
   subject_alternative_names = ["${var.root_domain_name}"]
+}
+
+resource "aws_cloudfront_distribution" "www_distribution" {
+  origin {
+    custom_origin_config {
+      // These are all the defaults.
+      http_port              = "80"
+      https_port             = "443"
+      origin_protocol_policy = "http-only"
+      origin_ssl_protocols   = ["TLSv1", "TLSv1.1", "TLSv1.2"]
+    }
+
+    domain_name = "${aws_s3_bucket.www.website_endpoint}"
+    origin_id   = "${var.www_domain_name}"
+  }
+
+  enabled             = true
+  default_root_object = "index.html"
+
+  default_cache_behavior {
+    viewer_protocol_policy = "redirect-to-https"
+    compress               = true
+    allowed_methods        = ["GET", "HEAD"]
+    cached_methods         = ["GET", "HEAD"]
+    target_origin_id       = "${var.www_domain_name}"
+    min_ttl                = 0
+    default_ttl            = 86400
+    max_ttl                = 31536000
+
+    forwarded_values {
+      query_string = false
+      cookies {
+        forward = "none"
+      }
+    }
+  }
+
+  aliases = ["${var.www_domain_name}"]
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  viewer_certificate {
+    acm_certificate_arn = "${aws_acm_certificate.certificate.arn}"
+    ssl_support_method  = "sni-only"
+  }
 }
