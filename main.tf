709bd98f96fcba369feeb30be1fe2baa3255f613
Jan Hein Hoogstad <yeehaa@offcourse.io>
2020-07-18 23:59:39 +0200

    more clean up template

--- main.tf
+++ main.tf
@@ -111,13 +111,13 @@ resource "aws_cloudfront_distribution" "www_distribution" {
 }
 
 resource "aws_route53_record" "www" {
-  zone_id = "${aws_route53_zone.zone.zone_id}"
+  zone_id = aws_route53_zone.zone.zone_id
   name    = var.subdomain_name
   type    = "A"
 
   alias {
-    name                   = "${aws_cloudfront_distribution.www_distribution.domain_name}"
-    zone_id                = "${aws_cloudfront_distribution.www_distribution.hosted_zone_id}"
+    name                   = aws_cloudfront_distribution.www_distribution.domain_name
+    zone_id                = aws_cloudfront_distribution.www_distribution.hosted_zone_id
     evaluate_target_health = false
   }
 }
