7ee7c9cbba2e32ce3d927f76ab5e6855467241c2
Jan Hein Hoogstad <yeehaa@offcourse.io>
2020-07-18 22:30:44 +0200

    add certificate (FIX)

--- main.tf
+++ main.tf
@@ -55,7 +55,7 @@ POLICY
 resource "aws_acm_certificate" "certificate" {
   // We want a wildcard cert so we can host subdomains later.
   domain_name       = "*.${var.root_domain_name}"
-  validation_method = "EMAIL"
+  validation_method = "DNS"
 
   // We also want the cert to be valid for the root domain even though we'll be
   // redirecting to the www. domain immediately.
