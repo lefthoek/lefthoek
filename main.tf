e36ae7172659d4c76d93e335d9f881da79d5402b
Jan Hein Hoogstad <yeehaa@offcourse.io>
2020-07-18 21:56:53 +0200

    add bucket

--- main.tf
+++ main.tf
@@ -10,8 +10,44 @@ terraform {
   }
 }
 
-resource "null_resource" "example" {
-  triggers = {
-    value = "A example resource that does nothing!"
+variable "www_domain_name" {
+  default = "brandbook.lefthoek.com"
+}
+
+// We'll also need the root domain (also known as zone apex or naked domain).
+variable "root_domain_name" {
+  default = "lefthoek.com"
+}
+
+
+resource "aws_s3_bucket" "www" {
+  bucket = "${var.www_domain_name}"
+  acl    = "public-read"
+  // We also need to create a policy that allows anyone to view the content.
+  // This is basically duplicating what we did in the ACL but it's required by
+  // AWS. This post: http://amzn.to/2Fa04ul explains why.
+  policy = <<POLICY
+{
+  "Version":"2012-10-17",
+  "Statement":[
+    {
+      "Sid":"AddPerm",
+      "Effect":"Allow",
+      "Principal": "*",
+      "Action":["s3:GetObject"],
+      "Resource":["arn:aws:s3:::${var.www_domain_name}/*"]
+    }
+  ]
+}
+POLICY
+
+  // S3 understands what it means to host a website.
+  website {
+    // Here we tell S3 what to use when a request comes in to the root
+    // ex. https://www.runatlantis.io
+    index_document = "index.html"
+    // The page to serve up if a request results in an error or a non-existing
+    // page.
+    error_document = "404.html"
   }
 }
