3fd9f3efc1f6e9dce2ae5e1c112f81ae3ff4febc
Jan Hein Hoogstad <yeehaa@offcourse.io>
2020-07-19 17:27:50 +0200

    refactor website to module

--- main.tf
+++ main.tf
@@ -16,45 +16,13 @@ provider "aws" {
 }
 
 
-variable "subdomain_name" {
-  default = "brandbook.lefthoek.com"
-}
-
 variable "root_domain_name" {
   default = "lefthoek.com"
-}
-
-
-resource "aws_s3_bucket" "www" {
-  bucket = var.subdomain_name
-  acl    = "public-read"
-  policy = <<POLICY
-{
-  "Version":"2012-10-17",
-  "Statement":[
-    {
-      "Sid":"AddPerm",
-      "Effect":"Allow",
-      "Principal": "*",
-      "Action":["s3:GetObject"],
-      "Resource":["arn:aws:s3:::${var.subdomain_name}/*"]
-    }
-  ]
-}
-POLICY
-
-  website {
-    index_document = "index.html"
-    // The page to serve up if a request results in an error or a non-existing
-    // page.
-    error_document = "404.html"
-  }
 }
 
 resource "aws_route53_zone" "zone" {
   name = var.root_domain_name
 }
-
 
 resource "aws_acm_certificate" "certificate" {
   domain_name               = "*.${var.root_domain_name}"
@@ -62,62 +30,11 @@ resource "aws_acm_certificate" "certificate" {
   subject_alternative_names = [var.root_domain_name]
 }
 
-resource "aws_cloudfront_distribution" "www_distribution" {
-  origin {
-    custom_origin_config {
-      http_port              = "80"
-      https_port             = "443"
-      origin_protocol_policy = "http-only"
-      origin_ssl_protocols   = ["TLSv1", "TLSv1.1", "TLSv1.2"]
-    }
-
-    domain_name = aws_s3_bucket.www.website_endpoint
-    origin_id   = var.subdomain_name
-  }
-
-  enabled             = true
-  default_root_object = "index.html"
-
-  default_cache_behavior {
-    viewer_protocol_policy = "redirect-to-https"
-    compress               = true
-    allowed_methods        = ["GET", "HEAD"]
-    cached_methods         = ["GET", "HEAD"]
-    target_origin_id       = var.subdomain_name
-    min_ttl                = 0
-    default_ttl            = 86400
-    max_ttl                = 31536000
-
-    forwarded_values {
-      query_string = false
-      cookies {
-        forward = "none"
-      }
-    }
-  }
-
-  aliases = [var.subdomain_name]
-
-  restrictions {
-    geo_restriction {
-      restriction_type = "none"
-    }
-  }
-
-  viewer_certificate {
-    acm_certificate_arn = aws_acm_certificate.certificate.arn
-    ssl_support_method  = "sni-only"
-  }
-}
-
-resource "aws_route53_record" "www" {
+module "website" {
+  source = "./infra/website"
+  subdomain_prefix = "brandbook"
+  root_domain_name = var.root_domain_name
+  certificate_arn = aws_acm_certificate.certificate.arn
   zone_id = aws_route53_zone.zone.zone_id
-  name    = var.subdomain_name
-  type    = "A"
-
-  alias {
-    name                   = aws_cloudfront_distribution.www_distribution.domain_name
-    zone_id                = aws_cloudfront_distribution.www_distribution.hosted_zone_id
-    evaluate_target_health = false
-  }
 }
+
