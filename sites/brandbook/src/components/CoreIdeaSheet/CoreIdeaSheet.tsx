f1a33910325262847d70744f37f881dae968480a
Jan Hein Hoogstad <yeehaa@offcourse.io>
2020-08-22 00:19:26 +0200

    add core idea sheet

---
+++ sites/brandbook/src/components/CoreIdeaSheet/CoreIdeaSheet.tsx
@@ -0,0 +1,54 @@
+/** @jsx jsx */
+import { jsx, Card, Box, Heading } from "theme-ui";
+import { FunctionComponent } from "react";
+import { capitalize } from "voca";
+
+type CoreIdea = {
+  goal: string;
+  idea: string;
+  context: string[];
+  usps: string[];
+  requirements: string[];
+};
+
+const sectionStyle = {
+  p: 3,
+  bg: "muted",
+};
+
+const sheetStyle = {
+  display: "grid",
+  gridTemplateColumns: "3fr 4fr",
+  gridTemplateRows: "repeat(3, 1fr)",
+  gridGap: 3,
+  ".context": {
+    gridRow: "2/4",
+  },
+};
+
+const CoreIdeaSheet: FunctionComponent<CoreIdea> = (props) => {
+  return (
+    <Card sx={sheetStyle}>
+      {Object.entries(props).map(([key, value]) => {
+        return (
+          <Box key={key} className={key} sx={sectionStyle}>
+            <Heading sx={{ mb: 3 }} variant="smallHeading">
+              {capitalize(key)}
+            </Heading>
+            {typeof value === "string" ? (
+              value
+            ) : (
+              <ul>
+                {value.map((item) => {
+                  return <li>{item}</li>;
+                })}
+              </ul>
+            )}
+          </Box>
+        );
+      })}
+    </Card>
+  );
+};
+
+export { CoreIdeaSheet };
