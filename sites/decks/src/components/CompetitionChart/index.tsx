589bc7e1508fa1dbbe8333f6781045ab6f6d3dc2
Jan Hein Hoogstad <yeehaa@offcourse.io>
2020-08-29 23:32:47 +0200

    add competitive analysis

--- sites/decks/src/components/CompetitionChart/index.tsx
+++ sites/decks/src/components/CompetitionChart/index.tsx
@@ -2,11 +2,12 @@
 import { FunctionComponent } from "react";
 import { jsx, useThemeUI, Box, Heading } from "theme-ui";
 import {
-  Cell,
+  Legend,
   ScatterChart,
   Scatter,
   XAxis,
   YAxis,
+  ZAxis,
   Label,
   LabelList,
   ReferenceLine,
@@ -14,16 +15,22 @@ import {
   ResponsiveContainer,
 } from "recharts";
 
-const CompetitionChart: FunctionComponent<{ data: any[] }> = ({ data }) => {
+const CompetitionChart: FunctionComponent<{ data: any }> = ({ data }) => {
   const { theme } = useThemeUI();
   // @ts-ignore
+  const {
+    lefthoek,
+    conversationalAI,
+    knowledgeManagementTools,
+    virtualAssistants,
+  } = data;
   const colors = theme.colors;
   return (
     <Box
       sx={{
         height: "100%",
         width: "100%",
-        maxHeight: ["80vh", "100%"],
+        maxHeight: ["85vh", "100%"],
         display: "flex",
         flexDirection: "column",
       }}
@@ -32,11 +39,11 @@ const CompetitionChart: FunctionComponent<{ data: any[] }> = ({ data }) => {
         Competition
       </Heading>
       <ResponsiveContainer
-        sx={{ maxWidth: ["100%", "80vw"], alignSelf: "center" }}
+        sx={{ fontSize: 0, maxWidth: ["100%", "80vw"], alignSelf: "center" }}
       >
         <ScatterChart>
           <XAxis
-            domain={[-1, 1]}
+            domain={[-10, 10]}
             padding={{ left: 15, right: 15 }}
             hide={true}
             type="number"
@@ -44,27 +51,27 @@ const CompetitionChart: FunctionComponent<{ data: any[] }> = ({ data }) => {
             name="automation"
           />
           <YAxis
-            domain={[-1, 1]}
+            domain={[-10, 10]}
             padding={{ top: 15, bottom: 15 }}
             hide={true}
             type="number"
             dataKey={"importance"}
             name="importance"
           />
+          <ZAxis dataKey={"relevance"} range={[500, 500]} name="scope" />
+
           <ReferenceArea
             x1={0}
-            x2={1}
+            x2={10}
             y1={0}
-            y2={1}
+            y2={10}
             opacity={0.5}
             fill={colors.secondary}
           />
-          <ReferenceLine y={0} opacity={0.5} stroke={colors.secondary} />
-          <ReferenceLine x={0} opacity={0.5} stroke={colors.secondary} />
-          <ReferenceLine x={-1} opacity={0}>
+          <ReferenceLine x={-10} opacity={0}>
             <Label value="manual" angle={-90} opacity={0.6} position="center" />
           </ReferenceLine>
-          <ReferenceLine x={1} opacity={0}>
+          <ReferenceLine x={10} opacity={0}>
             <Label
               value="automated"
               angle={90}
@@ -72,27 +79,48 @@ const CompetitionChart: FunctionComponent<{ data: any[] }> = ({ data }) => {
               position="center"
             />
           </ReferenceLine>
-          <ReferenceLine y={1} opacity={0}>
+          <ReferenceLine y={10} opacity={0}>
             <Label value="core business" opacity={0.6} position="center" />
           </ReferenceLine>
-          <ReferenceLine y={-1} opacity={0}>
-            <Label value="chores" opacity={0.6} position="center" />
+          <ReferenceLine y={-10} opacity={0}>
+            <Label value="mundane tasks" opacity={0.6} position="center" />
           </ReferenceLine>
           <Scatter
-            name="A school"
-            line={false}
-            data={data}
-            fill={colors.primary}
-            shape={() => null}
+            shape="star"
+            name="Lefthoek"
+            data={lefthoek}
+            fill={colors.accent}
           >
-            {data.map((entry, index) => (
-              <Cell
-                key={`cell-${index}`}
-                fill={entry.highlight ? colors.primary : colors.text}
-              />
-            ))}
-            <LabelList dataKey="name" />
+            <LabelList offset={10} position="bottom" dataKey="name" />
           </Scatter>
+          <Scatter
+            legendType="square"
+            shape="square"
+            name="Conversational AI"
+            data={conversationalAI}
+            fill={colors.text}
+          >
+            <LabelList offset={10} position="bottom" dataKey="name" />
+          </Scatter>
+          <Scatter
+            legendType="circle"
+            shape="circle"
+            name="Knowledge Management Tools"
+            data={knowledgeManagementTools}
+            fill={colors.primary}
+          >
+            <LabelList offset={10} position="bottom" dataKey="name" />
+          </Scatter>
+          <Scatter
+            legendType="triangle"
+            shape="triangle"
+            name="Virtual Assistants"
+            data={virtualAssistants}
+            fill={colors.secondary}
+          >
+            <LabelList offset={10} position="bottom" dataKey="name" />
+          </Scatter>
+          <Legend height={50} verticalAlign="top" />
         </ScatterChart>
       </ResponsiveContainer>
     </Box>
