999d42ad6b6945f48342f0c1e736e4602fd57700
Jan Hein Hoogstad <yeehaa@offcourse.io>
2020-09-15 00:52:04 +0200

    wip: togglable display container

--- sites/brandbook/src/components/DisplayContainer/index.tsx
+++ sites/brandbook/src/components/DisplayContainer/index.tsx
@@ -1,21 +1,34 @@
 /** @jsx jsx */
+import { useState } from "react";
 import { jsx, Box } from "theme-ui";
 import { FunctionComponent } from "react";
+import { Background } from "@lefthoek/atoms";
 
 const DisplayContainer: FunctionComponent<{ colorMode: string }> = ({
   children,
   colorMode = "muted",
 }) => {
+  const [isFullScreen, setFullScreenMode] = useState(false);
+  const baseStyles = { bg: colorMode };
   return (
-    <Box
-      sx={{
-        bg: colorMode,
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "stretch",
-      }}
+    <Background
+      sx={
+        isFullScreen
+          ? {
+              ...baseStyles,
+              position: "fixed",
+              top: 0,
+              overflowY: "scroll",
+              zIndex: 999,
+              bottom: 0,
+              left: 0,
+              right: 0,
+            }
+          : baseStyles
+      }
     >
       <Box
+        onClick={() => setFullScreenMode((current) => !current)}
         sx={{
           alignSelf: "center",
           display: ["grid"],
@@ -26,7 +39,7 @@ const DisplayContainer: FunctionComponent<{ colorMode: string }> = ({
       >
         {children}
       </Box>
-    </Box>
+    </Background>
   );
 };
 export { DisplayContainer };
