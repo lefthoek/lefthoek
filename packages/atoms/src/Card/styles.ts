ec6987d280c7fd7f146fd21a940b4f80e27ecba7
Jan Hein Hoogstad <yeehaa@offcourse.io>
2020-09-15 00:37:53 +0200

    wip: cards

--- packages/atoms/src/Card/styles.ts
+++ packages/atoms/src/Card/styles.ts
@@ -7,8 +7,16 @@ const textColor = {
   lobster: "muted",
 };
 
+const boxShadows = {
+  skyBlue: "0px 40px 80px #C9B7FD;",
+  brightGreen: "0px 40px 80px rgba(167, 243, 208, 0.8)",
+  midnight: "0px 40px 80px rgba(173, 173, 184, 0.6)",
+  lobster: "0px 40px 80px #EDBDA4",
+};
+
 export const overrideStyles: (args: { variant: string }) => SxStyleProp = ({
   variant,
 }) => ({
   color: textColor[variant],
+  boxShadow: boxShadows[variant],
 });
