9589596a4a8102e5198e4acd94d9a29b0fc6330a
Jan Hein Hoogstad <yeehaa@offcourse.io>
2020-09-23 11:30:09 +0200

    move google fonts to css

--- sites/landing/gatsby-config.js
+++ sites/landing/gatsby-config.js
@@ -3,16 +3,6 @@ module.exports = {
     siteName: `Generic Site`,
   },
   plugins: [
-    {
-      resolve: `gatsby-plugin-google-fonts`,
-      options: {
-        fonts: [
-          `spartan\:400,500,600,700`, // you can also specify font weights and styles
-          `crimson Text\:400`,
-        ],
-        display: "swap",
-      },
-    },
     `gatsby-plugin-typescript`,
     {
       resolve: `gatsby-plugin-compile-es6-packages`,
