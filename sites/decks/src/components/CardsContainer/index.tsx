4fed348de1302ef4fd06779abc33249a876239f7
Jan Hein Hoogstad <yeehaa@offcourse.io>
2020-10-14 12:43:55 +0200

    set maxwidth for text

---
+++ sites/decks/src/components/CardsContainer/index.tsx
@@ -0,0 +1,18 @@
+/** @jsx jsx */
+import { FunctionComponent } from "react";
+import { jsx, Box } from "theme-ui";
+
+const CardsContainer: FunctionComponent = ({ children }) => {
+  return (
+    <Box
+      sx={{
+        display: ["grid"],
+        gridTemplateColumns: ["auto","1fr 1fr"],
+        gridGap: [5],
+      }}
+    >
+      {children}
+    </Box>
+  );
+};
+export { CardsContainer };
