e42581923636e5fdfc6d8c890479dbdf8e57d9cc
Jan Hein Hoogstad <yeehaa@offcourse.io>
2020-11-01 21:36:12 +0100

    cut corner

--- sites/landing/src/sections/AboutSection/styles.ts
+++ sites/landing/src/sections/AboutSection/styles.ts
@@ -2,22 +2,50 @@ import { SxStyleProp } from "theme-ui";
 
 export const outerWrapperStyles: SxStyleProp = {
   display: "flex",
-  bg: "accent",
-  color: "text",
-  px: [6, 6, 7],
+  bg: "muted",
+  pl: [0, 0],
+  pr: [0, 0],
+  pt: [0, 0],
+  pb: [0, 0],
   alignItems: ["center", "center"],
   h2: {
     maxWidth: "30rem",
   },
   ".innerWrapper": {
-    maxWidth: "80rem",
-    borderBottomLeftRadius: "4px",
+    maxWidth: "100%",
+    py: [8, 9, 9],
+    px: [6, 6, 7],
+    width: "100%",
+    height: "100%",
+    bg: "accent",
+    color: "text",
+    clipPath: [
+      `polygon(
+    0 0%,
+    100% 0,
+    100% 0,
+    100% 0,
+    100% calc(100% - 50px),
+    0 100%,
+    100% 100%,
+    0 calc(100% - 0px)
+  )`,
+      `polygon(
+    0 0%,
+    100% 0,
+    100% 0,
+    100% 0,
+    100% calc(100% - 100px),
+    0 100%,
+    100% 100%,
+    0 calc(100% - 0px)
+  )`,
+    ],
   },
 };
 
 export const contentWrapperStyles: SxStyleProp = {
   display: "grid",
-  gridGap: [4],
   maxWidth: "40rem",
   gridTemplateColumns: ["auto", "1fr"],
 };
