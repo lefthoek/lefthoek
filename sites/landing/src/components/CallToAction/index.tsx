425e076e686be266b6e08f1f4645c9f69efa742f
Jan Hein Hoogstad <yeehaa@offcourse.io>
2020-11-03 13:21:21 +0100

    minor refactor

--- sites/landing/src/components/CallToAction/index.tsx
+++ sites/landing/src/components/CallToAction/index.tsx
@@ -1,9 +1,18 @@
 /** @jsx jsx */
-import { FunctionComponent, useEffect, FormEvent } from "react";
+import { FunctionComponent, FormEvent } from "react";
 import { Input } from "@lefthoek/atoms";
 import { jsx, Button, Heading } from "theme-ui";
 import { useForm } from "@formspree/react";
 import { outerWrapperStyles, inputStyles, buttonStyles } from "./styles";
+
+const tagEvent = (tag: string) => {
+  // @ts-ignore
+  if (!window.plausible) {
+    console.log("DEV", tag);
+  }
+  // @ts-ignore
+  window.plausible(tag, { callback: console.log("PROD", tag) });
+};
 
 const CallToAction: FunctionComponent<{
   className?: string;
@@ -12,14 +21,9 @@ const CallToAction: FunctionComponent<{
   variant?: string;
 }> = ({ className, callToAction, variant = "midnight" }) => {
   const [state, handleSubmit] = useForm("contactForm");
-  const handleSubmitX = (e: FormEvent) => {
+  const handleSubmitForm = (e: FormEvent) => {
     handleSubmit(e);
-    // @ts-ignore
-    if (!window.plausible) {
-      console.log("DEV", "SIGN UP");
-    }
-    // @ts-ignore
-    window.plausible("SIGN UP", { callback: console.log("PROD", "SIGN UP") });
+    tagEvent("SIGN UP");
   };
   const buttonsVariants = {
     skyBlue: "midnight",
@@ -32,7 +36,7 @@ const CallToAction: FunctionComponent<{
 
   return (
     <form
-      onSubmit={handleSubmitX}
+      onSubmit={handleSubmitForm}
       className={className}
       sx={outerWrapperStyles}
     >
