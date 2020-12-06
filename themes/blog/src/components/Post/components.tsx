d01e5fa1d6841104edfae18acbe76480a80faa1d
Jan Hein Hoogstad <yeehaa@offcourse.io>
2020-12-06 14:21:02 +0100

    wip: lists

--- themes/blog/src/components/Post/components.tsx
+++ themes/blog/src/components/Post/components.tsx
@@ -22,6 +22,25 @@ const blockquoteStyles = {
   },
 };
 
+const ulStyles = {
+  listStyleType: "'»'",
+  my: 6,
+  p: 0,
+  mx: 3,
+};
+
+const liStyles = {
+  listStylePosition: "outside",
+  "::marker": {
+    color: "secondary",
+  },
+  mb: 3,
+  "> p": {
+    position: "relative",
+    left: "1rem",
+  },
+};
+
 const pStyles = {
   mb: 4,
 };
@@ -54,10 +73,12 @@ const preStyles = {
 
 const a = (props) => <a sx={aStyles} {...props} />;
 const pre = comp("pre", preStyles);
+const ul = comp("ul", ulStyles);
+const li = comp("li", liStyles);
 const p = comp("p", pStyles);
 const h2 = comp("h2", h2Styles);
 const h3 = comp("h3", h3Styles);
 const hr = comp("hr", hrStyles);
 const blockquote = comp("blockquote", blockquoteStyles);
 
-export { blockquote, p, pre, h2, h3, a, hr };
+export { blockquote, p, pre, ul, li, h2, h3, a, hr };
