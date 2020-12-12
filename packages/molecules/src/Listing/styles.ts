0ecfd78dbc4457fc05c28e2da66643a7e584da86
Jan Hein Hoogstad <yeehaa@offcourse.io>
2020-12-12 22:03:40 +0100

    improvements to packages

--- packages/molecules/src/Listing/styles.ts
+++
@@ -1,5 +0,0 @@
-import { SxStyleProp } from "theme-ui";
-
-export const itemStyles: SxStyleProp = {
-    px: [4, 8, 9, "16rem"],
-};
---
+++ sites/brandbook/docs/components/atoms/Checkbox/Checkbox.tsx
@@ -0,0 +1,3 @@
+import { Checkbox } from "@lefthoek/atoms";
+
+export { Checkbox };
