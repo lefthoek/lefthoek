8cbe859c670c066b1a1b58ffbc1136efebd9c1af
Jan Hein Hoogstad <yeehaa@offcourse.io>
2021-04-23 21:14:17 +0200

    wip: workshop deck

--- sites/decks/src/slides/Agenda/index.tsx
+++ sites/decks/src/slides/Agenda/index.tsx
@@ -1,6 +1,6 @@
 /** @jsx jsx */
 import { FunctionComponent, ReactNodeArray } from "react";
-import { jsx, Box } from "theme-ui";
+import { jsx, Box, Heading } from "theme-ui";
 import { Basic } from "../Basic";
 import { innerWrapperStyles, outerWrapperStyles } from "./styles";
 
@@ -9,13 +9,13 @@ const Agenda: FunctionComponent<{
   index: number;
   className?: string;
 }> = ({ children, className, index }) => {
-  const [header, ...rest] = children;
+  const [...rest] = children;
   return (
     <Basic className={className} index={index} sx={outerWrapperStyles}>
-      <Box sx={innerWrapperStyles}>
-        {header}
-        <ul>{rest}</ul>
-      </Box>
+      <Heading as="h1" variant="display">
+        Agenda
+      </Heading>
+      <Box sx={innerWrapperStyles}>{rest}</Box>
     </Basic>
   );
 };
--- sites/decks/src/slides/Agenda/styles.ts
+++ sites/decks/src/slides/Agenda/styles.ts
@@ -1,44 +1,22 @@
-import {SxStyleProp} from "theme-ui";
+import { SxStyleProp } from "theme-ui";
 
 const outerWrapperStyles: SxStyleProp = {
-  px: [5, 5, 8, 8],
+    px: [5, 5, 8, 8],
 };
 
 const innerWrapperStyles: SxStyleProp = {
-  display: "flex",
-  maxWidth: "60rem",
-  flexDirection: ["column"],
-  h1: {
-    mb: 0,
-  },
-  li: {
-    listStylePosition: "outside",
-    "::marker": {
-      fontSize: ["42px", "58px"],
-      lineHeight: ["44px", "62px"],
-      fontWeight: 900,
-      fontStyle: "super-italic",
-      fontFamily: "heading",
-      color: "secondary",
-    },
-    "> p": {
-      left: "0rem",
-    },
-  },
-  "> ul": {
-    m: [0],
-    p: [0],
-    textIndent: [0],
-    listStyleType: [0],
-    display: ["flex"],
-    flex: 1,
-    justifyContent: ["center", "center"],
-    alignItems: ["center", "center"],
+    display: "flex",
+    maxWidth: "60rem",
     flexDirection: ["column"],
-    li: {
-      mb: [3],
+    h1: {
+        textAlign: ["center", "center"],
+        mb: [6, 6, 7, 7],
     },
-  },
+    justifyContent: ["start"],
+    alignItems: ["start"],
+    p: {
+        fontFamily: "monospace",
+    },
 };
 
-export {innerWrapperStyles, outerWrapperStyles};
+export { innerWrapperStyles, outerWrapperStyles };
