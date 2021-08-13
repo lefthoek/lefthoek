a2fbe2ddc42b695a3e98bd2a249e00b57e23ae0c
Jan Hein Hoogstad <yeehaa@offcourse.io>
2021-08-13 22:38:24 +0200

    wip: slack integration

--- sites/landing/src/sections/FooterSection/index.tsx
+++ sites/landing/src/sections/FooterSection/index.tsx
@@ -2,13 +2,14 @@
 import { FunctionComponent } from "react";
 import { jsx, Box } from "theme-ui";
 import { Section } from "../../components";
-import { LinkedIn, Email, Blog } from "./icons";
+import { LinkedIn, Email, Blog, Slack } from "./icons";
 import { iconStyles, iconWrapperStyles, outerWrapperStyles } from "./styles";
 
 const FooterSection: FunctionComponent = () => {
   return (
     <Section sx={outerWrapperStyles}>
       <Box sx={iconWrapperStyles}>
+        <Slack sx={iconStyles} />
         <Blog sx={iconStyles} />
         <Email email="contact@lefthoek.com" sx={iconStyles} />
         <LinkedIn url="https://www.linkedin.com/in/yeehaa/" sx={iconStyles} />
